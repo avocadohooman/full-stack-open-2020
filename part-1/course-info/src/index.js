c0ab4c48d05cea74761904289af6371d2090697b
gmolin <gmolin@student.hive.fi>
2020-11-21 14:51:30 +0200

    course-info ex 1.3-1.5 added

--- part-1/course-info/src/index.js
+++ part-1/course-info/src/index.js
@@ -16,9 +16,9 @@ const Content = (props) => {
 
 	return (
 		<>
-			<Part part={props.parts[0]} exercise={props.exercises[0]}/>
-			<Part part={props.parts[1]} exercise={props.exercises[1]}/>
-			<Part part={props.parts[2]} exercise={props.exercises[2]}/>
+			<Part part={props.parts[0].name} exercise={props.parts[0].exercises}/>
+			<Part part={props.parts[1].name} exercise={props.parts[1].exercises}/>
+			<Part part={props.parts[2].name} exercise={props.parts[2].exercises}/>
 		</>
 	)
 }
@@ -41,27 +41,28 @@ const Total = (props) => {
 }
 
 const App = () => {
-	const course = 'Half Stack application development'
-	const exercise1 = 10
-	const exercise2 = 7
-	const exercise3 = 14
-	const totalAmountOfExercises = exercise1 + exercise2 + exercise3
-	const parts = [
-		'Fundamental of React',
-		'Using props to pass data',
-		'State of a component'
-	]
-	const exercises = [
-		exercise1,
-		exercise2,
-		exercise3
-	]
-
+	const course = {
+		name: 'Half Stack application development',
+		parts: [
+			{
+				name: 'Fundamental of React',
+				exercises: 10
+			},
+			{
+				name: 'Using props to pass data',
+				exercises: 7
+			},
+			{
+				name: 'State of a component',
+				exercises: 14
+			}
+		]
+	}
 	return (
 		<>
-			<Header course={course}/>
-			<Content parts={parts} exercises = {exercises}/>
-			<Total totalAmountOfExercises={totalAmountOfExercises}/>
+			<Header course={course.name}/>
+			<Content parts = {course.parts}/>
+			<Total totalAmountOfExercises={course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises}/>
 		</>
 	)
 }
