4ea52053fba268f35a0b5c349a48c04a98efd721
gmolin <gmolin@student.hive.fi>
2020-11-29 15:28:25 +0200

    setting up part-2 phonbook exercises

---
+++ part-2/phonebook/src/components/app.js
@@ -0,0 +1,12 @@
+import React, {useState} from 'react'
+
+const App = () => {
+
+	return (
+		<div>
+			<p>42</p>
+		</div>
+	)
+}
+
+export default App
---
+++ part-2/phonebook/src/index.js
@@ -0,0 +1,10 @@
+import React, {useState} from 'react'
+import ReactDOM from 'react-dom'
+import App from './components/app'
+
+
+
+ReactDOM.render(
+	<App />,
+	document.getElementById('root')
+)
