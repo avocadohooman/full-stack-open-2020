68dc1fcad615b0f920cc1353529ea43ee2342128
gmolin <gmolin@student.hive.fi>
2021-04-03 14:12:19 +0300

    creating users

--- part-3/practice/controllers/users.js
+++ part-3/practice/controllers/users.js
@@ -2,10 +2,18 @@ const bcrypt = require('bcrypt')
 const usersRouter = require('express').Router()
 const User = require('../models/user')
 
+
+usersRouter.get('/', async (request, response) => {
+    const users = await User.find({})
+
+    response.json(users);
+})
+
 usersRouter.post('/', async (request, response) => {
     const body = request.body;
 
     const saltRounds = 10;
+
     const passwordHash = await bcrypt.hash(body.password, saltRounds);
 
     const user = new User({
