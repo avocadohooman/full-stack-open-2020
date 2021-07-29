3727f4e34d92cfa56b717cdc07d19ec77b20de43
gerhard.molin <gmolin@student.hive.fi>
2021-07-29 10:12:37 +0300

    adding user and login functionality

--- part-8/practice/graphql-server/index.js
+++ part-8/practice/graphql-server/index.js
@@ -1,11 +1,15 @@
 const { ApolloServer, UserInputError, gql } = require('apollo-server');
 const { v1: uuid } = require('uuid')
 const Person = require('./models/person');
+const User = require('./models/User');
 const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken')
 require('dotenv').config();
 
 const MONGODB_URI = process.env.MONGODB_URI;
 console.log('connecting to: ', MONGODB_URI);
+
+const JWT_SECRET = process.env.SECRET
 
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
   .then(() => {
@@ -13,31 +17,7 @@ mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true,
   })
   .catch((error) => {
     console.log('error connection to MongoDB:', error.message)
-  })
-
-let persons = [
-    {
-        name: "Arto Hellas",
-        phone: "040-123543",
-        street: "Tapiolankatu 5 A",
-        city: "Espoo",
-        id: "3d594650-3436-11e9-bc57-8b80ba54c431"
-      },
-      {
-        name: "Matti Luukkainen",
-        phone: "040-432342",
-        street: "Malminkaari 10 A",
-        city: "Helsinki",
-        id: '3d599470-3436-11e9-bc57-8b80ba54c431'
-      },
-      {
-        name: "Venla Ruuska",
-        street: "Nallemäentie 22 C",
-        city: "Helsinki",
-        id: '3d599471-3436-11e9-bc57-8b80ba54c431'
-      },
-];
-
+});
 
 const typeDefs = gql
 `
@@ -53,6 +33,16 @@ const typeDefs = gql
         id: ID!
     }
 
+    type User {
+        username: String!
+        friends: [Person!]!
+        id: ID!
+    }
+
+    type Token {
+        value: String!
+    }
+
     enum YesNo {
         YES
         NO
@@ -62,6 +52,7 @@ const typeDefs = gql
         personCount: Int!
         allPersons(phone: YesNo): [Person!]!
         findPerson(name: String!): Person
+        me: User
     }
 
     type Mutation {
@@ -75,6 +66,13 @@ const typeDefs = gql
             name: String!
             phone: String!
         ): Person
+        createUser(
+            username: String!
+        ): User
+        login(
+            username: String!
+            password: String!
+        ): Token
     }
 `
 
@@ -87,9 +85,12 @@ const resolvers = {
             }
             return Person.find({ phone: { $exists: args.phone === 'YES'  }})
         },
-        findPerson: (root, args) => 
+        findPerson: (root, args) => {
             Person.findOne({ name: args.name })
-    },
+        },
+        me: (root, args, context) => {
+            return context.currentUser;
+        },
     Person: {
         name: (root) => root.name,
         phone: (root) => root.phone,
@@ -132,13 +133,43 @@ const resolvers = {
                 })
             }
             return person
-        }
+        },
+        createUser: (root, args) => {
+            const user = new User({username: args.username})
+            try {
+                user.save();
+            } catch (error) {
+                throw new UserInputError(error.message, {
+                    invalidArgs: args,
+                })
+            }
+            return user;
+        },
+        login: async (root, args) => {
+            const user = await User.findOne({ username: args.username });
+            if (!user || args.password !== 'secret') {
+                throw new UserInputError("wrong credentials");
+            }
+            const userForToken = {
+                username: user.username,
+                id: user._id
+            }
+            return { value: jwt.sign(userForToken, JWT_SECRET)};
+        },
     }
 }
 
 const server = new ApolloServer({
     typeDefs,
-    resolvers
+    resolvers,
+    context: async ({req}) => {
+        const auth = req ? req.headers.authorization : null;
+        if (auth && auth.toLocaleLowerCase().startsWith('bearer')) {
+            const decodedToken = jwt.verify(auth.substring(7), JWT_SECRET);
+        }
+        const currentUser = await User.findById(decodedToken.id).populate('friends');
+        return { currentUser }
+    }
 });
 
 
