8034c42aa2d8a081f605bb2b56076a18a9a32037
avocadohooman <gmolin@student.hive.fi>
2021-08-25 17:04:35 +0300

    back in the game with react native

--- part-10/rate-repository-app/App.js
+++ part-10/rate-repository-app/App.js
@@ -1,21 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Pressable, Alert } from 'react-native';
+import Main from './src/components/Main';
 
-export default function App() {
+const App = () => {
+  console.log('I love console.log() for debugging');
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Main />
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
