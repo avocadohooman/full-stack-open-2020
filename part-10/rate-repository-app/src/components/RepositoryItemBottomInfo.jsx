404234abdc39c4a97085a0a1c8fdf53342e4be1d
avocadohooman <gmolin@student.hive.fi>
2021-08-27 11:12:52 +0300

    part-10 ex 8 done

--- part-10/rate-repository-app/src/components/RepositoryItemBottomInfo.jsx
+++ part-10/rate-repository-app/src/components/RepositoryItemBottomInfo.jsx
@@ -9,6 +9,7 @@ const styles = StyleSheet.create({
         justifyContent: 'space-between',
         marginTop: 20,
         marginLeft: 20,
+        marginRight: 20,
         marginBottom: 10,
     },
     bottomColumn: {
--- part-10/rate-repository-app/src/components/SignIn.jsx
+++ part-10/rate-repository-app/src/components/SignIn.jsx
@@ -1,9 +1,73 @@
 import React from 'react';
-
+import { Pressable, View, StyleSheet } from 'react-native';
 import Text from './Text';
+import { Formik } from 'formik';
+import FormikTextInput from './FormikTextInput';
+import theme from '../theme';
+
+const initialValues = {
+  username: '',
+  password: '',
+};
+
+const styles = StyleSheet.create({
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    flexGrow: 1,
+  },  
+  userCredentials: {
+    height: 50,
+    borderWidth: 1,
+    borderRadius: 5,
+    borderColor: theme.colors.textSecondary,
+    marginTop: 20,
+    marginLeft: 10,
+    marginRight: 10,
+    paddingLeft: 10,
+  },
+  signIn: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 50,
+    borderRadius: 5,
+    backgroundColor: theme.colors.primary,
+    marginTop: 20,
+    marginLeft: 10,
+    marginRight: 10,
+    paddingLeft: 10,
+  },
+});
+
+const UserCredentialForm = ({onSubmit}) => {
+
+  return (
+    <View style={styles.container}>
+      <FormikTextInput style={styles.userCredentials} name={'username'} placeholder={'Username'} />
+      <FormikTextInput style={styles.userCredentials} name={'password'} placeholder={'Password'} />
+      <Pressable onPress={onSubmit}>
+        <View style={styles.signIn}>
+          <Text style={styles.signInLabel} color={'white'} fontWeight={'bold'}>Sign In</Text>
+        </View>
+      </Pressable>
+    </View>
+  );
+};
 
 const SignIn = () => {
-  return <Text>The sign in view</Text>;
+  const onSubmit = values => {
+    console.log(values);
+  };
+
+  return (
+    <View>
+        <Formik initialValues={initialValues} onSubmit={onSubmit}>
+          {({handleSubmit}) => <UserCredentialForm onSubmit={handleSubmit}/>}
+        </Formik>
+    </View>
+  );
 };
 
 export default SignIn;
